b87c6ab2fd52da4293f8dd1b7f6a116c4536abf4
sohithsunny80 <100835615+sohithsunny80@users.noreply.github.com>
2022-03-03 17:06:20 +0530

    Update sunny.java

--- sunny.java
+++ sunny.java
@@ -12,6 +12,8 @@ public static void main(String[] args){
           System.out.println("hello brother....");
           System.out.println("hello sistaaa....");
           System.out.println("hello mummy...");
+            System.out.println("hello mummy...");
+
 
 
 
