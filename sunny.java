e8c59e30fe2bad71eebde89209a1998bb30d167b
sohithsunny80 <100835615+sohithsunny80@users.noreply.github.com>
2022-03-03 17:40:36 +0530

    Update sunny.java

--- sunny.java
+++ sunny.java
@@ -16,6 +16,8 @@ public static void main(String[] args){
               System.out.println("hello puka...");
               System.out.println("hello puka...");
 
+                System.out.println("hello sunny...");
+
 
 
 
