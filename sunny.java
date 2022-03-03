223d9a093b5d545fc5b3d3a361d37e22f0e33e3c
sohithsunny80 <100835615+sohithsunny80@users.noreply.github.com>
2022-03-03 17:00:29 +0530

    Create sunny.java

---
+++ sunny.java
@@ -0,0 +1,20 @@
+public class sunny{
+public static void main(String[] args){
+{
+  System.out.println("hello sohith");
+
+    System.out.println("hello sunny");
+  
+      System.out.println("hello broo....");
+      System.out.println("hello sohith kumar....");
+        System.out.println("hello sohith kumar....");
+
+          System.out.println("hello brother....");
+          System.out.println("hello sistaaa....");
+          System.out.println("hello mummy...");
+
+
+
+}
+}
+}
